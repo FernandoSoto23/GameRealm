fa320f8a34ce7f10ff2fa968f4902bd333d22c49
fernando soto <fernandosoto23@hotmail.com>
2023-06-01 22:05:35 -0700

    ruta servicioxd

--- src/clases/rutas.tsx
+++ src/clases/rutas.tsx
@@ -2,4 +2,4 @@
 const ServidorLocal = "https://localhost:7092";
 const ServidorAzure = "https://sekyhwebservice.azurewebsites.net"
 
-export const WebServiceUrl = ServidorLocal;
+export const WebServiceUrl = ServidorAzure;
