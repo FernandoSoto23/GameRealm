e63009e4e7c8c1c63510a546f1ad26f36154c74c
SKYNEXTGG <143541113+SKYNEXTGG@users.noreply.github.com>
2023-11-23 10:31:36 -0700

    Se agrego el header

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -1,41 +1,45 @@
 import path from "path";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-
-//menu fixed
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartArrowDown, faCartShopping, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import sm from '../assets/GM.svg'
 
 
 export function Header() {
-    const [nombreUsuario,setNombreUsuario] = useState("");
+    const [nombreUsuario, setNombreUsuario] = useState("");
 
-    const MostrarUsuario = ()=>{
-        let usuarioGameRealm  : any = localStorage.getItem("UsuarioGameRealm");
+    const MostrarUsuario = () => {
+        let usuarioGameRealm: any = localStorage.getItem("UsuarioGameRealm");
         usuarioGameRealm = JSON.parse(usuarioGameRealm);
-        if(usuarioGameRealm !== null){
+        if (usuarioGameRealm !== null) {
             setNombreUsuario(usuarioGameRealm["nombreUsuario"]);
         }
     }
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         MostrarUsuario();
-    },[]);
-    
+    }, []);
+
     let variable = 10;
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleDropdown = () => {
-      setIsOpen(!isOpen);
+        setIsOpen(!isOpen);
     };
     return (
         <header className="header">
             <nav className="header-navegacion">
                 <div className="buscar-producto">
-                    <input className="input" type="text" placeholder="Buscar" />    
-                    <button className="boton boton-buscar-"></button>
+                    {/* Agregar el SVG junto a la barra de búsqueda */}
+                    <img src={sm} alt="Logo" className="logo-svg" />
+
+                    <input className="input" type="text" placeholder="Buscar" />                 
                 </div>
 
                 <div>
                     <ul className="header-ul">
+
                         <NavLink to="/Home" className={(({ isActive }) => isActive ? 'activo' : '')}>
                             <li className="boton header-botones">
 
@@ -48,7 +52,7 @@ export function Header() {
                                 <p>Biblioteca</p>
                             </li>
                         </NavLink>
-                        
+
                         <NavLink to="/Biblioteca" className={(({ isActive }) => isActive ? 'activo' : '')}>
                             <li className="boton header-botones">
                                 <p>Categorias</p>
@@ -56,18 +60,20 @@ export function Header() {
                         </NavLink>
                     </ul>
                 </div>
- 
+
                 <div className="card-login-carrito">
+
+                    {nombreUsuario !== "" ? <NavLink to="./Perfil">{nombreUsuario}</NavLink> : <NavLink to="./Login" style={{ color: 'white' }}><FontAwesomeIcon icon={faRightFromBracket}></FontAwesomeIcon>Iniciar Sesion</NavLink>}
                     <div>
                         <a className="carrito">
-                            <div className="position-relative">
-                                <img src="https://i.ibb.co/fQwSFC5/carro-de-la-carretilla.png" alt="carro-de-la-carretilla"/>
-                                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger bg-num-carrrito">{variable}</span>
-                            </div>
+                            <NavLink to={"./Carrito"}>
+                                <div className="position-relative">
+                                    <FontAwesomeIcon icon={faCartShopping} style={{ color: 'white' }}></FontAwesomeIcon>
+                                </div>
+                            </NavLink>
+
                         </a>
-                    </div> 
-                    {nombreUsuario !== "" ? <NavLink to="./Perfil">{nombreUsuario}</NavLink> : <NavLink to="./Login">Iniciar Sesion</NavLink>}
-                    
+                    </div>
                 </div>
             </nav>
 
